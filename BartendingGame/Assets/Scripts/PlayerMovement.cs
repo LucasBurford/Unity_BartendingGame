53e284942cc8b8bb94c768a8ac6419131d6e444a
Lucas-Burford <lucasburford@live.co.uk>
2021-12-19 18:44:45 +0000

    Added placeholder player and movement

---
+++ BartendingGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Fields
+    [Header("Gameplay and spec")]
+
+    // The move speed of the player
+    [SerializeField]
+    private float moveSpeed;
+    public float MoveSpeed
+    {
+        get { return moveSpeed; }
+        set { moveSpeed = value; }
+    }
+
+    #endregion
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gather horizontal inputs
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        // Store them in a Vector3
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+        // Apply Vector3 to position
+        transform.position += moveDirection * moveSpeed;
+    }
+}
